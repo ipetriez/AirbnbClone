c2ae1eafe3f6b7e881ac29d9439740359974c812
Sergey Petrosyan <ipetriez@icloud.com>
2023-12-26 19:43:34 +0400

    Initial Commit

---
+++ AirbnbClone/AirbnbCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirbnbCloneApp.swift
+//  AirbnbClone
+//
+//  Created by Sergey Petrosyan on 26.12.23.
+//
+
+import SwiftUI
+
+@main
+struct AirbnbCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
