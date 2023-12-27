9dc4a6080698140f7046fbff92dac5d6825b3758
Sergey Petrosyan <ipetriez@icloud.com>
2023-12-27 19:10:59 +0400

    Create ExploreView. Build skeleton of the Explore screen.

---
+++ AirbnbClone/Explore/ExploreView.swift
@@ -0,0 +1,18 @@
+//
+//  ExploreView.swift
+//  AirbnbClone
+//
+//  Created by Sergey Petrosyan on 27.12.23.
+//
+
+import SwiftUI
+
+struct ExploreView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExploreView()
+}
