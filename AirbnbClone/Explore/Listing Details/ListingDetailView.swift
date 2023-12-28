ae5770b54f3bcba70d49c5fb020689646038795d
Sergey Petrosyan <ipetriez@icloud.com>
2023-12-28 23:47:00 +0400

    Build wireframe for ListingDetailView.

---
+++ AirbnbClone/Explore/Listing Details/ListingDetailView.swift
@@ -0,0 +1,204 @@
+//
+//  ListingDetailView.swift
+//  AirbnbClone
+//
+//  Created by Sergey Petrosyan on 28.12.23.
+//
+
+import SwiftUI
+import MapKit
+
+struct ListingDetailView: View {
+    @Environment(\.dismiss) var dismiss
+    var body: some View {
+        ScrollView {
+            ZStack(alignment: .topLeading) {
+                ImageSliderView()
+                    .frame(height: 320)
+                
+                Button(action: {
+                    dismiss()
+                }, label: {
+                    Image(systemName: "chevron.left")
+                        .padding(32)
+                        .foregroundStyle(.black)
+                        .background {
+                            Circle()
+                                .fill(.white)
+                                .frame(width: 32, height: 32)
+                        }
+                })
+            }
+            
+            VStack(alignment: .leading, spacing: 8) {
+                Text("Miami Villa")
+                    .font(.title)
+                    .fontWeight(.semibold)
+                
+                VStack(alignment: .leading) {
+                    HStack(spacing: 2) {
+                        ItemRatingView(rating: 4.85)
+                        Text(" — ")
+                        Text("28 reviews")
+                            .fontWeight(.semibold)
+                            .underline()
+                    }
+                    .foregroundStyle(.black)
+                    
+                    Text("Miami, Florida")
+                }
+                .font(.caption)
+            }
+            .padding(.leading)
+            .frame(maxWidth: .infinity, alignment: .leading)
+            
+            Divider()
+            
+            HStack {
+                VStack(alignment: .leading, spacing: 4) {
+                    Text("Entire villa hosted by James Smith")
+                        .font(.headline)
+                        .frame(width: 250, alignment: .leading)
+                    
+                    HStack(spacing: 2) {
+                        Text("4 guests —")
+                        Text("4 bedrooms —")
+                        Text("4 beds —")
+                        Text("3 baths")
+                    }
+                    .font(.caption)
+                }
+                .frame(width: 300, alignment: .leading)
+                
+                Spacer()
+                
+                Image("profile-photo")
+                    .resizable()
+                    .frame(width: 64, height: 64)
+                    .scaledToFill()
+                    .clipShape(.circle)
+            }
+            .padding(.horizontal)
+            
+            Divider()
+            
+            VStack(spacing: 0) {
+                ForEach(0 ..< 2, id: \.self) { feature in
+                    HStack {
+                        Image(systemName: "medal")
+                        VStack(alignment: .leading) {
+                            Text("Superhost")
+                                .font(.footnote)
+                                .fontWeight(.semibold)
+                            
+                            Text("Superhosts are experienced, highly rated hosts, who are committed to providing great stars for guests.")
+                                .font(.caption)
+                                .foregroundStyle(.gray)
+                        }
+                    }
+                    .padding()
+                }
+            }
+            
+            Divider()
+            
+            VStack(alignment: .leading, spacing: 16) {
+                Text("Where you'll sleep")
+                    .font(.headline)
+                ScrollView(.horizontal, showsIndicators: false) {
+                    HStack(spacing: 16) {
+                        ForEach(1 ..< 5, id: \.self) { bedroom in
+                            VStack {
+                                Image(systemName: "bed.double")
+                                
+                                Text("Bedroom \(bedroom)")
+                            }
+                        }
+                        .frame(width: 132, height: 100)
+                        .overlay {
+                            RoundedRectangle(cornerRadius: 12)
+                                .stroke(lineWidth: 1)
+                                .foregroundStyle(.gray)
+                        }
+                    }
+                }
+                .scrollTargetBehavior(.paging)
+            }
+            .padding()
+            
+            Divider()
+            
+            VStack(alignment: .leading, spacing: 16) {
+                Text("What does this place offer?")
+                    .font(.headline)
+                
+                ForEach(0 ..< 5, id: \.self) { feature in
+                    HStack {
+                        Image(systemName: "wifi")
+                            .frame(width: 32)
+                        
+                        Text("WiFi")
+                            .font(.footnote)
+                        
+                        Spacer()
+                    }
+                }
+            }
+            .padding()
+            
+            Divider()
+            
+            VStack(alignment: .leading, spacing: 16) {
+                Text("Where you'll be")
+                    .font(.headline)
+                
+                Map()
+                    .frame(height: 200)
+                    .clipShape(RoundedRectangle(cornerRadius: 12))
+            }
+            .padding()
+        }
+        .ignoresSafeArea()
+        .padding(.bottom, 72)
+        .overlay(alignment: .bottom) {
+            VStack {
+                Divider()
+                    .padding(.bottom)
+                
+                HStack {
+                    VStack(alignment: .leading) {
+                        Text("$500.00")
+                            .font(.subheadline)
+                            .fontWeight(.semibold)
+                        
+                        Text("Total before taxes")
+                            .font(.footnote)
+                        
+                        Text("Oct 15 - 20")
+                            .font(.footnote)
+                            .fontWeight(.semibold)
+                            .underline()
+                    }
+                    
+                    Spacer()
+                    
+                    Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                        Text("Reserve")
+                            .foregroundStyle(.white)
+                            .font(.subheadline)
+                            .fontWeight(.semibold)
+                            .frame(width: 140, height: 40)
+                            .background(.pink)
+                            .clipShape(RoundedRectangle(cornerRadius: 8))
+                    })
+                }
+                .padding(.horizontal, 32)
+            }
+            .background(.white)
+        }
+    }
+}
+
+#Preview {
+    ListingDetailView()
+}
--- AirbnbClone/Explore/View/ExploreView.swift
+++ AirbnbClone/Explore/View/ExploreView.swift
@@ -23,7 +23,8 @@ struct ExploreView: View {
                 }
             }
             .navigationDestination(for: Int.self) { listing in
-                Text("Listing detail view: \(listing)")
+                ListingDetailView()
+                    .navigationBarBackButtonHidden()
             }
         }
     }
