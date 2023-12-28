e54af9d47dabae536dbcc4ea3b29d1c4bc9b1023
Sergey Petrosyan <ipetriez@icloud.com>
2023-12-28 15:50:08 +0400

    Add sample images. Implement slider inside of each item on explore screen.

--- AirbnbClone/Explore/View/Components/Listings/View/ListingItemView.swift
+++ AirbnbClone/Explore/View/Components/Listings/View/ListingItemView.swift
@@ -8,11 +8,26 @@
 import SwiftUI
 
 struct ListingItemView: View {
+    var imageNames = [
+        "listing-1",
+        "listing-2",
+        "listing-3",
+        "listing-4",
+        "listing-5"
+    ]
+    
     var body: some View {
         VStack(spacing: 8) {
-             Rectangle()
-                .frame(height: 320)
-                .clipShape(RoundedRectangle(cornerRadius: 10))
+            TabView {
+                ForEach(imageNames, id: \.self) { image in
+                    Image(image)
+                        .resizable()
+                        .scaledToFit()
+                }
+            }
+            .frame(height: 320)
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+            .tabViewStyle(.page)
             
             HStack(alignment: .top, spacing: 0) {
                 VStack(alignment: .leading) {
